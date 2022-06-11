8f5b0a2e60c774682eb2eb28e68996b6de6823df
Pedro <pedrohenriquesk10@yahoo.com>
2022-06-11 19:44:53 -0300

    Juros OK

--- teste.cpp
+++ teste.cpp
@@ -1,14 +1,20 @@
-#include <time.h>
-#include <stdio.h>
 #include <iostream>
+#include <ctime>
+#include <chrono>
 
 using namespace std;
+using namespace chrono;
 
 int main(){
 
-    time_t mytime;
-    mytime = time(NULL);
-    cout << ctime(&mytime) << endl;
+    time_t t;
+    time(&t);
+
+    cout << localtime(&t)->tm_mday << endl;
+
+    cout << (localtime(&t)->tm_mon + 1) << endl;
+
+    cout << (localtime(&t)->tm_year + 1900) << endl;
 
     return 0;
 }
