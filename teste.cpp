6279b6165008c1db5f0265a0507e79808ca43f03
Pedro <pedrohenriquesk10@yahoo.com>
2022-06-11 17:31:21 -0300

    nova branch Pedro

--- teste.cpp
+++ teste.cpp
@@ -1,6 +1,14 @@
-#include <iostream>
 #include <time.h>
+#include <stdio.h>
+#include <iostream>
 
-int main() {
-    Now();
+using namespace std;
+
+int main(){
+
+    time_t mytime;
+    mytime = time(NULL);
+    cout << ctime(&mytime) << endl;
+
+    return 0;
 }
