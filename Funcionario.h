0db4a47b9f70592ec68b2fa8321681326186908c
Hélder <magalhaes.helder@hotmail.com>
2022-06-11 21:00:22 -0300

    classes Funcionario.h, Usuario.h, Leiturista.h alteradas

--- Funcionario.h
+++ Funcionario.h
@@ -3,11 +3,18 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include "Usuario.h"
 
 using namespace std;
 
 class Funcionario : public Usuario {
+    private:
+    string id;
+    vector<Servico*> servicos;
 
-}
+    public:
+    Funcionario();
+
+};
 
 #endif
